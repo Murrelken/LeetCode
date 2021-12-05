6c9ab21564d1b455315aeaabb647fd252991e655
butin <butin@hightech.group>
2021-12-05 22:24:55 +0300

    Simplify 704

--- LeetCode/LeetCode/OtherActivities/AlgorithmStudyPlan/Algorithm1/Problem704.cs
+++ LeetCode/LeetCode/OtherActivities/AlgorithmStudyPlan/Algorithm1/Problem704.cs
@@ -4,34 +4,25 @@ namespace LeetCode.OtherActivities.AlgorithmStudyPlan.Algorithm1
     {
         public int Search(int[] nums, int target)
         {
-            var index = nums.Length / 2;
-            var left = 0;
-            var right = nums.Length;
-            var shouldSkipCheck = nums.Length == 2;
+            int left = 0, index;
+            var right = nums.Length - 1;
             while (true)
             {
+                index = (right + left) / 2;
                 if (nums[index] == target)
                     return index;
 
-                if (right - left < 2 && !shouldSkipCheck)
+                if (left > right)
                     return -1;
-
-                shouldSkipCheck = false;
 
                 if (nums[index] > target)
                 {
-                    right = index;
-                    index = (right + left) / 2;
-                    if (index == right)
-                        index--;
+                    right = index - 1;
                     
                 }
                 else
                 {
-                    left = index;
-                    index = (right + left) / 2;
-                    if (index == left)
-                        index++;
+                    left = index + 1;
                 }
             }
         }
