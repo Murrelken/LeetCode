20d4df8634e0da78db07c48a0af57cd21d710922
butin <butin@hightech.group>
2021-12-08 02:31:40 +0300

    add 557 index out of range

---
+++ LeetCode/LeetCode/OtherActivities/AlgorithmStudyPlan/Algorithm1/Problem557.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using Enumerable = System.Linq.Enumerable;
+
+namespace LeetCode.OtherActivities.AlgorithmStudyPlan.Algorithm1
+{
+    public class Problem557
+    {
+        public string ReverseWords(string s)
+        {
+            short left, right;
+            left = right = 0;
+            var stringArray = s.ToArray();
+
+            while (right <= s.Length)
+            {
+                if (right == s.Length || s[right] == ' ')
+                {
+                    Array.Reverse(stringArray, left, right - left);
+                    left = (short) (right + 1);
+                }
+
+                right++;
+            }
+            
+            return new string(stringArray);
+        }
+    }
+}
