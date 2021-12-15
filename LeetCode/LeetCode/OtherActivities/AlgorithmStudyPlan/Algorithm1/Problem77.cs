2c8239cd306a9df4fd660d220c03edde2a1a89e2
butin <butin@hightech.group>
2021-12-16 00:42:57 +0300

    add 77

---
+++ LeetCode/LeetCode/OtherActivities/AlgorithmStudyPlan/Algorithm1/Problem77.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.OtherActivities.AlgorithmStudyPlan.Algorithm1
+{
+    public class Problem77
+    {
+        public IList<IList<int>> Combine(int maxNumber, int resultsLength)
+        {
+            var result = new List<IList<int>>();
+            Helper(maxNumber, resultsLength, 1, new List<int>(), result);
+        
+            return result;
+        }
+    
+        private void Helper(int n, int k, int i, List<int> l, List<IList<int>> res)
+        {
+            if (l.Count == k)
+                res.Add(new List<int>(l));
+            else
+                for (var j = i; j <= n; j++)
+                {
+                    l.Add(j);
+                
+                    Helper(n, k, j + 1, l, res);
+                
+                    l.RemoveAt(l.Count - 1);
+                }
+        }
+    }
+}
