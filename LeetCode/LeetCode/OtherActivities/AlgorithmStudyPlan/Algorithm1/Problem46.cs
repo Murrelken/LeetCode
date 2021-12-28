74aa6b74eeb348e93f21be18dece01274a5b4ac4
butin <butin@hightech.group>
2021-12-28 19:59:11 +0300

    improve 46

--- LeetCode/LeetCode/OtherActivities/AlgorithmStudyPlan/Algorithm1/Problem46.cs
+++ LeetCode/LeetCode/OtherActivities/AlgorithmStudyPlan/Algorithm1/Problem46.cs
@@ -9,22 +9,25 @@ namespace LeetCode.OtherActivities.AlgorithmStudyPlan.Algorithm1
         {
             var result = new List<IList<int>>();
 
-            Helper(new List<int>(), nums, result);
+            Helper(new List<int>(), nums.ToList(), result);
 
             return result;
         }
 
-        private static void Helper(List<int> tempList, int[] availableNums, List<IList<int>> result)
+        private static void Helper(List<int> tempList, List<int> availableNums, List<IList<int>> result)
         {
-            if (availableNums.Length == 0)
+            if (availableNums.Count == 0)
                 result.Add(tempList.ToList());
             else
             {
-                for (var i = 0; i < availableNums.Length; i++)
+                for (var i = 0; i < availableNums.Count; i++)
                 {
-                    tempList.Add(availableNums[i]);
+                    var item = availableNums[i];
+                    tempList.Add(item);
                     
-                    Helper(tempList, availableNums.Where((_, index) => index != i).ToArray(), result);
+                    availableNums.RemoveAt(i);
+                    Helper(tempList, availableNums, result);
+                    availableNums.Insert(i, item);
                     
                     tempList.RemoveAt(tempList.Count - 1);
                 }
