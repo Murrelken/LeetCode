806adf51d0a4b4d912378f22c3ee9db31fec9c98
butin <butin@hightech.group>
2023-06-12 02:31:47 +0400

    add 1351

---
+++ LeetCode/LeetCode/Problems/1/Problem1351.cs
@@ -0,0 +1,24 @@
+namespace LeetCode.Problems;
+
+public class Problem1351
+{
+    public int CountNegatives(int[][] grid) {
+        var res=0;
+        var height=grid.Length;
+        var width=grid[0].Length;
+        var nextI=width-1;
+        for(var row=0;row<height;row++)
+        for(var i=nextI;i>=0;i--)
+        {
+            if(grid[row][i]>=0)
+            {
+                res+=(nextI-i)*(height-row);
+                nextI=i;
+                break;
+            }
+            if(i==0)
+                return res + (nextI+1)*(height-row);
+        }
+        return res;
+    }
+}
