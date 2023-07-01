f2507711a698e3fff38dfbcbc871e4ef1527d2af
Murrelken <danilbutin@gmail.com>
2023-07-01 19:29:27 +0200

    2305

---
+++ LeetCode/LeetCode/Problems/2/Problem2305.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace LeetCode.Problems;
+
+public class Problem2305
+{
+    public int DistributeCookies(int[] cookies, int k)
+    {
+        var bags = new int[k];
+
+        return Dfs(0, bags, cookies, k, k);
+    }
+
+    private int Dfs(int i, int[] bags, int[] cookies, int k, int zeroCount)
+    {
+        if (cookies.Length - i < zeroCount)
+            return int.MaxValue;
+
+        if (i == cookies.Length)
+            return bags.Max();
+
+        var answer = int.MaxValue;
+        for (var j = 0; j < k; ++j)
+        {
+            zeroCount -= bags[j] == 0 ? 1 : 0;
+            bags[j] += cookies[i];
+
+            answer = Math.Min(answer, Dfs(i + 1, bags, cookies, k, zeroCount));
+
+            bags[j] -= cookies[i];
+            zeroCount += bags[j] == 0 ? 1 : 0;
+        }
+
+        return answer;
+    }
+}
