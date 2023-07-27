4d38db73fe2c170a0c78bd9b85bb29e09c871ca7
Murrelken <danilbutin@gmail.com>
2023-07-27 18:31:00 +0200

    2142 rust .

--- leetcode_rust/src/problems/problem_2142.rs
+++ leetcode_rust/src/problems/problem_2142.rs
@@ -1,18 +1,18 @@
-pub fn max_run_time(n: i32, batteries: Vec<i32>) -> i64 {
-    let mut batteries = batteries.clone();
+pub fn max_run_time(n: i32, mut batteries: Vec<i32>) -> i64 {
     batteries.sort();
+    let n = n as usize;
     let mut extra = 0i64;
 
-    for i in 0..batteries.len() - n as usize {
+    for i in 0..batteries.len() - n {
         extra += batteries[i] as i64;
     }
 
-    let mut live: Vec<i32> = Vec::with_capacity(n as usize);
-    for i in batteries.len() - n as usize..batteries.len() {
+    let mut live: Vec<i32> = Vec::with_capacity(n);
+    for i in batteries.len() - n..batteries.len() {
         live.push(batteries[i]);
     }
 
-    for i in 0..(n - 1) as usize {
+    for i in 0..(n - 1) {
         if extra < (i + 1) as i64 * (live[i + 1] - live[i]) as i64 {
             return live[i] as i64 + extra / (i + 1) as i64;
         }
@@ -20,5 +20,5 @@ pub fn max_run_time(n: i32, batteries: Vec<i32>) -> i64 {
         extra -= (i + 1) as i64 * (live[i + 1] - live[i]) as i64;
     }
 
-    return live[n as usize - 1] as i64 + extra / n as i64;
+    return live[n - 1] as i64 + extra / n as i64;
 }
