60a8919ee20aa53bdb061b91686174e2b4af8bca
Murrelken <danilbutin@gmail.com>
2023-08-24 05:25:31 +0200

    rust helpers .

--- leetcode_rust/src/problems/helpers.rs
+++ leetcode_rust/src/problems/helpers.rs
@@ -1,5 +1,5 @@
 pub fn get_shift_of_rotated_vector<T: PartialOrd>(n: usize, nums: &Vec<T>) -> usize {
-    match nums.iter().zip(nums.iter().skip(1)).enumerate().find(|(i, (l, r))| l > r) {
+    match nums.iter().zip(nums.iter().skip(1)).enumerate().find(|(_, (l, r))| l > r) {
         Some((x, ..)) => { x + 1 }
         None => { n }
     }
