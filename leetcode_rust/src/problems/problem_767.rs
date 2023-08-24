41dcbc216b1230791eb34e2ebfde174fc0d02682
Murrelken <danilbutin@gmail.com>
2023-08-24 04:46:13 +0200

    add 767 rust v2

--- leetcode_rust/src/problems/problem_767.rs
+++ leetcode_rust/src/problems/problem_767.rs
@@ -3,6 +3,7 @@ use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
 use std::ops::DerefMut;
 use std::rc::Rc;
+use std::iter::repeat;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 struct CharWithWeight {
@@ -65,4 +66,47 @@ pub fn reorganize_string(s: String) -> String {
     } else {
         result.iter().collect()
     }
+}
+
+pub fn reorganize_string_v2(s: String) -> String {
+    let mut vec: Vec<_> = repeat(0)
+        .take(26)
+        .collect();
+
+    for c in s.chars() {
+        vec[char_to_index(c)] += 1;
+    }
+
+
+    let mut result: Vec<char> = Vec::new();
+
+    loop {
+        let (i, w) = vec
+            .iter()
+            .enumerate()
+            .filter(|(i, w)| result.is_empty() || result.last().unwrap() != &index_to_char(*i))
+            .max_by(|(_, w), (_, w2)| w.cmp(w2))
+            .unwrap();
+
+        if w == &0 {
+            break;
+        }
+
+        result.push(index_to_char(i));
+        vec[i] -=1;
+    }
+
+    if result.len() < s.len() {
+        String::new()
+    } else {
+        result.iter().collect()
+    }
+}
+
+fn index_to_char(i: usize) -> char {
+    (i as u8 + 'a' as u8) as char
+}
+
+fn char_to_index(c: char) -> usize {
+    (c as u8 - 'a' as u8) as usize
 }
