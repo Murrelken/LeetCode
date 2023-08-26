1c32863769d30d0c9aa1a67a1a12884330cf72e4
Murrelken <danilbutin@gmail.com>
2023-08-26 23:07:06 +0200

    add 646 rust version without recursion

--- leetcode_rust/src/problems/problem_646.rs
+++ leetcode_rust/src/problems/problem_646.rs
@@ -1,4 +1,20 @@
 pub fn find_longest_chain(mut pairs: Vec<Vec<i32>>) -> i32 {
+    pairs.sort_by(|x, y| x[1].cmp(&y[1]));
+    let mut prev_right = -1001;
+    let mut res = 0;
+
+    for pair in pairs.iter() {
+        if pair[0] > prev_right {
+            res += 1;
+            prev_right = pair[1];
+        }
+    }
+
+    res
+}
+
+/// Common with dp. Skip or take.
+pub fn find_longest_chain_v2(mut pairs: Vec<Vec<i32>>) -> i32 {
     pairs.sort_by(|x, y| x[0].cmp(&y[0]));
     let mut dp = vec![0; pairs.len()];
     let mut res = 0;
