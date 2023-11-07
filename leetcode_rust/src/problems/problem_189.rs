e2295dd5c5e11cf53846319bdd72b853aab9ba7b
Murrelken <danilbutin@gmail.com>
2023-11-08 00:18:40 +0100

    add another version of 189 rust

--- leetcode_rust/src/problems/problem_189.rs
+++ leetcode_rust/src/problems/problem_189.rs
@@ -27,3 +27,11 @@ pub fn rotate(nums: &mut Vec<i32>, k: i32) {
         index_to_insert += 1;
     }
 }
+
+pub fn rotate_with_reverses(nums: &mut Vec<i32>, k: i32) {
+    let n = nums.len();
+    let k = k as usize % n;
+    nums.reverse();
+    nums[0..k].reverse();
+    nums[k..n].reverse();
+}
