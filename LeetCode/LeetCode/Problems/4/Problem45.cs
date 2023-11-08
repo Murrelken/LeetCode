5724792ed025e4db77847639267048c9cef0c81d
Murrelken <danilbutin@gmail.com>
2023-11-08 23:58:58 +0100

    add 45 v2 csharp

--- LeetCode/LeetCode/Problems/4/Problem45.cs
+++ LeetCode/LeetCode/Problems/4/Problem45.cs
@@ -26,4 +26,24 @@ public class Problem45
 
         return dp[n - 1];
     }
+
+    public int JumpV2(int[] nums)
+    {
+        var n = nums.Length;
+        for (var i = 1; i < n; i++)
+        {
+            nums[i] = Math.Max(nums[i] + i, nums[i - 1]);
+        }
+
+        var ind = 0;
+        var res = 0;
+
+        while (ind < n - 1)
+        {
+            res++;
+            ind = nums[ind];
+        }
+
+        return res;
+    }
 }
