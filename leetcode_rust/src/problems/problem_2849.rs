81c321e07abec5619c1b4eae110e9a101d823ede
Murrelken <danilbutin@gmail.com>
2023-11-08 18:02:36 +0100

    add 2849 leetcode version rust

--- leetcode_rust/src/problems/problem_2849.rs
+++ leetcode_rust/src/problems/problem_2849.rs
@@ -8,3 +8,14 @@ pub fn is_reachable_at_time(sx: i32, sy: i32, fx: i32, fy: i32, t: i32) -> bool 
         x_diff.max(y_diff) <= t as u32
     }
 }
+
+pub fn is_reachable_at_time_leetcode_ver(sx: i32, sy: i32, fx: i32, fy: i32, t: i32) -> bool {
+    let x_diff = (sx - fx).abs();
+    let y_diff = (sy - fy).abs();
+
+    if x_diff == 0 && y_diff == 0 {
+        t != 1
+    } else {
+        x_diff.max(y_diff) <= t
+    }
+}
