ad8919de6a6b346b9aad7631f4ab0556eccb0087
Murrelken <danilbutin@gmail.com>
2023-11-09 00:40:01 +0100

    add 274 memory usage version csharp

--- LeetCode/LeetCode/Problems/2/Problem274.cs
+++ LeetCode/LeetCode/Problems/2/Problem274.cs
@@ -15,4 +15,25 @@ public class Problem274
         }
         return result;
     }
+
+    public int HIndexV2(int[] citations)
+    {
+        int n = citations.Length;
+        int[] buckets = new int[n + 1];
+        foreach (var c in citations)
+        {
+            if (c >= n)
+                buckets[n]++;
+            else
+                buckets[c]++;
+        }
+        int count = 0;
+        for (int i = n; i >= 0; i--)
+        {
+            count += buckets[i];
+            if (count >= i)
+                return i;
+        }
+        return 0;
+    }
 }
