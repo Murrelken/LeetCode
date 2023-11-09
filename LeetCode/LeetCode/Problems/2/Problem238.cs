b3ded0b85d0bbd5cfdfd283c0286388e2bab8301
Murrelken <danilbutin@gmail.com>
2023-11-09 21:12:15 +0100

    add 238 v2 csharp

--- LeetCode/LeetCode/Problems/2/Problem238.cs
+++ LeetCode/LeetCode/Problems/2/Problem238.cs
@@ -2,8 +2,9 @@ namespace LeetCode.Problems
 {
     public class Problem238
     {
-        public int[] ProductExceptSelf(int[] nums) {
-        
+        public int[] ProductExceptSelf(int[] nums)
+        {
+
             int prod = 1;
             var containsZero = false;
             int? firstZeroPose = null;
@@ -49,6 +50,44 @@ namespace LeetCode.Problems
             }
 
             return nums;
+        }
+
+        public int[] ProductExceptSelfV2(int[] nums)
+        {
+            var n = nums.Length;
+            var result = new int[n];
+            var prod = 1;
+            var zeroMet = false;
+            var firstZeroIndex = -1;
+
+            for (var i = 0; i < n; i++)
+            {
+                if (nums[i] == 0)
+                    if (zeroMet)
+                    {
+                        result[firstZeroIndex] = 0;
+                        break;
+                    }
+                    else
+                    {
+                        zeroMet = true;
+                        firstZeroIndex = i;
+                        result[i] = nums[i];
+                        prod = 0;
+                    }
+                else
+                    prod *= nums[i];
+            }
+
+            if (prod == 0)
+                return result;
+
+            for (var i = 0; i < n; i++)
+            {
+                result[i] = prod / nums[i];
+            }
+
+            return result;
         }
     }
 }
