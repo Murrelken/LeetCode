96081a1c3d51db5dbfaafea87c7dbd5645be7466
Murrelken <danilbutin@gmail.com>
2023-11-09 07:00:42 +0100

    add improved 1759 csharp

--- LeetCode/LeetCode/Problems/17/Problem1759.cs
+++ LeetCode/LeetCode/Problems/17/Problem1759.cs
@@ -2,39 +2,22 @@ using System.Linq;
 
 public class Problem1759
 {
-    private const int MOD = 1_000_000_007;
 
     public int CountHomogenous(string s)
     {
         var result = 0;
-        var current = s[0];
-        var count = 1;
-        foreach (var c in s.Skip(1))
+        var count = 0;
+        const int MOD = 1_000_000_007;
+        for (var i = 0; i < s.Length; i++)
         {
-            if (c ==current) 
+            if (i == 0 || s[i] == s[i - 1])
                 count++;
             else
-            {
-                result += GetSumOfUpToN(count);
-                result %= MOD;
-                current = c;
                 count = 1;
-            }
+
+            result = (result + count) % MOD;
         }
-        result += GetSumOfUpToN(count);
-        result %= MOD;
 
         return result;
-    }
-
-    private static int GetSumOfUpToN(int n)
-    {
-        var sum = 0;
-        for (var i = 1; i <= n; i++)
-        {
-            sum += i;
-            sum %= MOD;
-        }
-        return sum;
     }
 }
