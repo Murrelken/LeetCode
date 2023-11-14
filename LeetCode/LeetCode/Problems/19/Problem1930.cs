00d03acff544a1c6adbcc0a03171e1b75dd259ec
Murrelken <danilbutin@gmail.com>
2023-11-14 17:35:33 +0100

    add 1930 csharp

---
+++ LeetCode/LeetCode/Problems/19/Problem1930.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems._19
+{
+	internal class Problem1930
+	{
+		public int CountPalindromicSubsequence(string s)
+		{
+			var uniqueElementsWindow = new HashSet<char>();
+			var res = 0;
+			for (char ch = 'a'; ch <= 'z'; ch++)
+			{
+				var first = s.IndexOf(ch);
+				if (first == -1)
+					continue;
+				var last = s.LastIndexOf(ch);
+				if (last - first < 2)
+					continue;
+
+				uniqueElementsWindow.Clear();
+
+				for (var j = first + 1; j < last; j++)
+					uniqueElementsWindow.Add(s[j]);
+
+				res += uniqueElementsWindow.Count;
+			}
+
+			return res;
+		}
+	}
+}
