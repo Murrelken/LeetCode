0ca6e0cf4cdd73ce28972463f42d446cfc8f0a93
Murrelken <danilbutin@gmail.com>
2023-11-21 20:20:08 +0100

    add 1814 csharp

---
+++ LeetCode/LeetCode/Problems/18/Problem1814.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._18
+{
+	internal class Problem1814
+	{
+		public int CountNicePairs(int[] nums)
+		{
+			var revs = nums
+				.Select(x => x - Rev(x))
+				.ToArray();
+
+			var res = 0;
+			const int MOD = 1_000_000_007;
+			var dictionary = new Dictionary<int, int>();
+            foreach (var item in revs)
+            {
+				dictionary.TryAdd(item, 0);
+				var existing = dictionary[item];
+				res = (res + existing) % MOD;
+				dictionary[item]++;
+			}
+
+			return res;
+		}
+
+		public int Rev(int num)
+		{
+			var result = 0;
+
+			while (num > 0)
+			{
+				result = result * 10 + num % 10;
+				num /= 10;
+			}
+
+			return result;
+		}
+	}
+}
