9a4425e69f705707c8aeaab6fcb683a1488e39d6
Murrelken <danilbutin@gmail.com>
2023-11-22 17:06:06 +0100

    add 1424 csharp

---
+++ LeetCode/LeetCode/Problems/14/Problem1424.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._14
+{
+	internal class Problem1424
+	{
+		public int[] FindDiagonalOrder(IList<IList<int>> nums)
+		{
+			var ordered = new List<LinkedList<int>>();
+			
+			for (int i = 0; i < nums.Count; i++)
+			{
+				for (int j = 0; j < nums[i].Count; j++)
+				{
+					var n = i + j;
+					if (ordered.Count <= n)
+						ordered.Add(new LinkedList<int>());
+
+					ordered[n].AddFirst(nums[i][j]);
+				}
+			}
+
+			var res = ordered.SelectMany(x => x).ToArray();
+
+			return res;
+		}
+	}
+}
