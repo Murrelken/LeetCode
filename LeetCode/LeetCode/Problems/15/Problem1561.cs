7419f8fbf7639cb38cc1aea96b52df1d0ac4f905
Murrelken <danilbutin@gmail.com>
2023-11-24 02:00:12 +0100

    add 1561 csharp

---
+++ LeetCode/LeetCode/Problems/15/Problem1561.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.Problems._15
+{
+	internal class Problem1561
+	{
+		public int MaxCoins(int[] piles)
+		{
+			Array.Sort(piles);
+
+			var n = piles.Length;
+			var third = n / 3;
+			var currMultiplier = 1;
+			var res = 0;
+
+			while (third > 0)
+			{
+				res += piles[n - currMultiplier * 2];
+				currMultiplier++;
+				third--;
+			}
+
+			return res;
+		}
+	}
+}
