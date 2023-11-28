14d70849ccdba23560a30183976a6d6eb7cf1506
Murrelken <danilbutin@gmail.com>
2023-11-28 18:59:43 +0100

    add 2147 csharp

---
+++ LeetCode/LeetCode/Problems/21/Problem2147.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Problems._21
+{
+	internal class Problem2147
+	{
+		public int NumberOfWays(string corridor)
+		{
+			const int MOD = 1_000_000_007;
+			var result = 1L;
+			var lookingForSecondSeat = true;
+			var initialIndex = corridor.IndexOf('S') + 1;
+			var tempWallsOptions = 0L;
+
+			for (var i = initialIndex; i < corridor.Length; i++)
+			{
+				var current = corridor[i];
+				if (lookingForSecondSeat)
+				{
+					if (current == 'S') lookingForSecondSeat = false;
+					else continue;
+				}
+				else
+				{
+					tempWallsOptions++;
+					if (current == 'S')
+					{
+						lookingForSecondSeat = true;
+						result = (result % MOD * tempWallsOptions) % MOD;
+						tempWallsOptions = 0;
+					}
+				}
+			}
+
+			return lookingForSecondSeat ? 0 : (int)result;
+		}
+	}
+}
