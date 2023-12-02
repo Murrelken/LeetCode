1fc9d05c767da2403e497c7b4d09870f2aa19cb5
Murrelken <danilbutin@gmail.com>
2023-12-03 00:28:45 +0100

    add 1160 csharp

---
+++ LeetCode/LeetCode/Problems/11/Problem1160.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._11
+{
+	internal class Problem1160
+	{
+		public int CountCharacters(string[] words, string chars)
+		{
+			var map = new Dictionary<char, int>();
+			foreach (var c in chars)
+				if (!map.TryAdd(c, 1))
+					map[c]++;
+			var result = 0;
+			foreach (var word in words)
+			{
+				var tempMap = map.ToDictionary(x => x.Key, x => x.Value);
+				var wordIsOkaySoFar = true;
+				foreach (var c in word)
+				{
+					if (!tempMap.ContainsKey(c) || tempMap[c] == 0)
+					{
+						wordIsOkaySoFar = false;
+						break;
+					}
+					tempMap[c]--;
+				}
+
+				if (wordIsOkaySoFar)
+					result += word.Length;
+			}
+
+			return result;
+		}
+	}
+}
