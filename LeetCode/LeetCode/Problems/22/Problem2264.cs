deffef4f7b9d249b4aa80c197fbbfc7559b40b2c
Murrelken <danilbutin@gmail.com>
2023-12-04 12:18:48 +0100

    add 2264 csharp

---
+++ LeetCode/LeetCode/Problems/22/Problem2264.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeetCode.Problems._22
+{
+	internal class Problem2264
+	{
+		public string LargestGoodInteger(string num)
+		{
+			var max = -1;
+			for (var i = 0; i < num.Length - 2; i++)
+			{
+				var converted = int.Parse(num[i].ToString());
+				if (num[i] == num[i + 1] && num[i] == num[i + 2] && converted > max)
+					max = converted;
+			}
+			return max == -1 ? string.Empty : (max.ToString() + max + max);
+		}
+	}
+}
