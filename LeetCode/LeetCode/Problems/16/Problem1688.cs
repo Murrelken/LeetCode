3b2621cb9495584784beee7f4d7ab8438e62f3f1
Murrelken <danilbutin@gmail.com>
2023-12-05 14:15:17 +0100

    add 1688 csharp

---
+++ LeetCode/LeetCode/Problems/16/Problem1688.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Problems._16
+{
+	internal class Problem1688
+	{
+		public int NumberOfMatches(int n)
+		{
+			var res = 0;
+			while (n > 1)
+			{
+				res += n / 2;
+				n = n / 2 + n % 2;
+			}
+			return res;
+		}
+	}
+}
