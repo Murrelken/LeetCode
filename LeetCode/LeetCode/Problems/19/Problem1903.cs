5ed0f4f2ca6666807a1d477d421acc3b2fd9ecac
Murrelken <danilbutin@gmail.com>
2023-12-08 09:38:28 +0100

    add 1903 csharp

---
+++ LeetCode/LeetCode/Problems/19/Problem1903.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Problems._19
+{
+	internal class Problem1903
+	{
+		public string LargestOddNumber(string num)
+		{
+			var lastOdd = -1;
+
+			for (var i = num.Length - 1; i >= 0; i--)
+			{
+				if ((num[i] - '0') % 2 == 1)
+				{
+					lastOdd = i;
+					break;
+				}
+			}
+
+			return lastOdd == -1 ? string.Empty : num.Substring(0, lastOdd + 1);
+		}
+	}
+}
