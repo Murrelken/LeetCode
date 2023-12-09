fdefe976ffeb575f0708246e6d8beb5df17d268e
Murrelken <danilbutin@gmail.com>
2023-12-09 19:00:49 +0100

    add 94 csharp

---
+++ LeetCode/LeetCode/Problems/9/Problem94.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.DataStructures;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems._9
+{
+	internal class Problem94
+	{
+		public IList<int> InorderTraversal(TreeNode root)
+		{
+			var res = new List<int>();
+			RecursiveTraversal(root, res);
+			return res;
+		}
+
+		public void RecursiveTraversal(TreeNode node, List<int> result)
+		{
+			if (node.left != null)
+				RecursiveTraversal(node.left, result);
+
+			result.Add(node.val);
+
+			if (node.right != null)
+				RecursiveTraversal(node.right, result);
+		}
+	}
+}
