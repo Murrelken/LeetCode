7c460328dbf3b17cbccd935d064a41f7a6b5a951
Murrelken <danilbutin@gmail.com>
2023-12-12 10:28:08 +0100

    add 1464 csharp

---
+++ LeetCode/LeetCode/Problems/14/Problem1464.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems._14
+{
+	internal class Problem1464
+	{
+		public int MaxProduct(int[] nums)
+		{
+			var max = int.MinValue;
+			var nextMax = int.MinValue;
+
+            foreach (var item in nums)
+            {
+                if (item > max)
+				{
+					nextMax = max;
+					max = item;
+				}
+				else if (item > nextMax)
+				{
+					nextMax = item;
+				}
+            }
+
+			return (max - 1) * (nextMax - 1);
+        }
+	}
+}
