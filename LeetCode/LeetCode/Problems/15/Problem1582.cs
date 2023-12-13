418fff3bda741bf87436d5352ccb6904e2d77318
Murrelken <danilbutin@gmail.com>
2023-12-14 00:01:14 +0100

    actually add 1582 csharp

--- LeetCode/LeetCode/Problems/15/Problem1582.cs
+++ LeetCode/LeetCode/Problems/15/Problem1582.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCode.Problems._15
 {
 	internal class Problem1582
 	{
+		public int NumSpecial(int[][] mat)
+		{
+			var m = mat.Length;
+			var n = mat[0].Length;
+			var columnsWhereOnesExist = new HashSet<int>();
+			var columnsWhereOnesExistMoreThanOnce = new HashSet<int>();
+			var rowsWithOnlyOneOne = new List<(int i, int j)>();
+
+			for (var i = 0; i < m; i++)
+			{
+				var tempJ = -1;
+				var found = 0;
+				for (var j = 0; j < n; j++)
+				{
+					if (mat[i][j] == 1)
+					{
+						tempJ = j;
+						found++;
+						if (columnsWhereOnesExist.Contains(j))
+							columnsWhereOnesExistMoreThanOnce.Add(j);
+						else
+							columnsWhereOnesExist.Add(j);
+					}
+				}
+				if (found == 1)
+					rowsWithOnlyOneOne.Add((i, tempJ));
+			}
+
+			return rowsWithOnlyOneOne.Count(x => !columnsWhereOnesExistMoreThanOnce.Contains(x.j));
+		}
 	}
 }
