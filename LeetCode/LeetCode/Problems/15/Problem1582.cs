111d0e3098c49cf7344327080dbd890fce2d1e8e
Murrelken <danilbutin@gmail.com>
2023-12-14 00:04:22 +0100

    1582 csharp final version

--- LeetCode/LeetCode/Problems/15/Problem1582.cs
+++ LeetCode/LeetCode/Problems/15/Problem1582.cs
@@ -11,7 +11,7 @@ namespace LeetCode.Problems._15
 			var n = mat[0].Length;
 			var columnsWhereOnesExist = new HashSet<int>();
 			var columnsWhereOnesExistMoreThanOnce = new HashSet<int>();
-			var positionOfOneInRowsToCheck = new List<(int i, int j)>();
+			var positionOfOneInRowsToCheck = new List<int>();
 
 			for (var i = 0; i < m; i++)
 			{
@@ -30,10 +30,10 @@ namespace LeetCode.Problems._15
 					}
 				}
 				if (found == 1)
-					positionOfOneInRowsToCheck.Add((i, tempJ));
+					positionOfOneInRowsToCheck.Add(tempJ);
 			}
 
-			return positionOfOneInRowsToCheck.Count(x => !columnsWhereOnesExistMoreThanOnce.Contains(x.j));
+			return positionOfOneInRowsToCheck.Count(x => !columnsWhereOnesExistMoreThanOnce.Contains(x));
 		}
 	}
 }
