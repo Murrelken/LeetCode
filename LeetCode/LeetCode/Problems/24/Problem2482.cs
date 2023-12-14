ad37032eff305569ad7e919ca5b811c279ce35ff
Murrelken <danilbutin@gmail.com>
2023-12-15 00:08:30 +0100

    add 2482 csharp

---
+++ LeetCode/LeetCode/Problems/24/Problem2482.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problems._24
+{
+	internal class Problem2482
+	{
+		public int[][] OnesMinusZeros(int[][] grid)
+		{
+			var m = grid.Length;
+			var n = grid[0].Length;
+			var rowOnes = new int[m];
+			var rowZeroes = new int[m];
+			var columnOnes = new int[n];
+			var columnZeroes = new int[n];
+
+			for (var i = 0; i < m; i++)
+				for (var j = 0; j < n; j++)
+					if (grid[i][j] == 0)
+					{
+						rowZeroes[i]++;
+						columnZeroes[j]++;
+					}
+					else
+					{
+						rowOnes[i]++;
+						columnOnes[j]++;
+					}
+
+			for (var i = 0; i < m; i++)
+				for (var j = 0; j < n; j++)
+					grid[i][j] = rowOnes[i] + columnOnes[j] - rowZeroes[i] - columnZeroes[j];
+
+			return grid;
+		}
+	}
+}
