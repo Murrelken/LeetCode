c11f9953716d7d86cdf022eadc364a90db343c0e
Murrelken <danilbutin@gmail.com>
2023-12-18 21:24:55 +0100

    add 1913 csharp

---
+++ LeetCode/LeetCode/Problems/19/Problem1913.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Problems._19
+{
+	internal class Problem1913
+	{
+		public int MaxProductDifference(int[] nums)
+		{
+			var max = int.MinValue;
+			var nextMax = int.MinValue;
+			var min = int.MaxValue;
+			var nextMin = int.MaxValue;
+
+			foreach (var num in nums)
+			{
+				if (num < min)
+				{
+					nextMin = min;
+					min = num;
+				}
+				else if (num < nextMin)
+				{
+					nextMin = num;
+				}
+
+				if (num > max)
+				{
+					nextMax = max;
+					max = num;
+				}
+				else if (num > nextMax)
+				{
+					nextMax = num;
+				}
+			}
+
+			return max * nextMax - min * nextMin;
+		}
+	}
+}
