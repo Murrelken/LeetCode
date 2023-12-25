415b25a1ded8b6ae3536b1d27013268333768ddd
Murrelken <danilbutin@gmail.com>
2023-12-25 01:34:30 +0100

    add 91 csharp

--- LeetCode/LeetCode/Problems/17/Problem1758.cs
+++ LeetCode/LeetCode/Problems/17/Problem1758.cs
@@ -6,16 +6,12 @@ namespace LeetCode.Problems._17
 	{
 		public int MinOperations(string s)
 		{
-			int n = s.Length;
-			int operationsCount = 0;
+			var n = s.Length;
+			var operationsCount = 0;
 
 			for (int i = 0; i < n; i++)
-			{
 				if (s[i] - '0' != i % 2)
-				{
 					operationsCount++;
-				}
-			}
 
 			return Math.Min(operationsCount, n - operationsCount);
 		}
