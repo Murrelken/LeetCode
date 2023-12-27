6351d860d76f84963182248217636d70fd954292
Murrelken <danilbutin@gmail.com>
2023-12-27 02:40:44 +0100

    add 1578 csharp

---
+++ LeetCode/LeetCode/Problems/15/Problem1578.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LeetCode.Problems._15
+{
+	internal class Problem1578
+	{
+		public int MinCost(string colors, int[] neededTime)
+		{
+			var iteratingOverTheSameColor = false;
+			var max = int.MinValue;
+			var tempCost = 0;
+			var totalCost = 0;
+
+			for (var i = 0; i < colors.Length - 1; i++)
+			{
+				if (colors[i] == colors[i + 1] || iteratingOverTheSameColor)
+				{
+					max = Math.Max(max, neededTime[i]);
+					tempCost += neededTime[i];
+					iteratingOverTheSameColor = true;
+					if (colors[i] != colors[i + 1])
+					{
+						iteratingOverTheSameColor = false;
+						totalCost += tempCost - max;
+						tempCost = 0;
+						max = int.MinValue;
+					}
+				}
+			}
+
+			if (iteratingOverTheSameColor)
+			{
+				max = Math.Max(max, neededTime[^1]);
+				tempCost += neededTime[^1];
+				totalCost += tempCost - max;
+			}
+
+			return totalCost;
+		}
+	}
+}
