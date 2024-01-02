4e35ff0bcb1a6710d756df45793673109c304f18
Murrelken <danilbutin@gmail.com>
2024-01-02 19:07:20 +0100

    add 2610 csharp

---
+++ LeetCode/LeetCode/Problems/26/Problem2610.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems._26
+{
+	internal class Problem2610
+	{
+		public IList<IList<int>> FindMatrix(int[] nums)
+		{
+			List<IList<int>> result = new() { new List<int>() };
+			int[] frequency = new int[nums.Length + 1];
+			int maxFrequency = 0;
+
+			foreach (int num in nums)
+			{
+				int currentFrequency = frequency[num]++;
+				if (currentFrequency > maxFrequency)
+				{
+					maxFrequency = currentFrequency;
+					result.Add(new List<int> { num });
+				}
+				else
+					result[currentFrequency].Add(num);
+			}
+
+			return result;
+		}
+	}
+}
