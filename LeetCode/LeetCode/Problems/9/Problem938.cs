6291551f81e5c532bad3ce2d2b5a5c2c118ee18b
Murrelken <danilbutin@gmail.com>
2024-01-08 20:10:27 +0100

    add 938 csharp

---
+++ LeetCode/LeetCode/Problems/9/Problem938.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Problems._9
+{
+	internal class Problem938
+	{
+		public int RangeSumBST(TreeNode root, int low, int high)
+		{
+			return Dfs(root, low, high);
+		}
+
+		private static int Dfs(TreeNode node, int min, int max)
+		{
+			var result = 0;
+			if (node == null) return result;
+			if (node.val < max)
+				result += Dfs(node.right, min, max);
+			if (node.val > min)
+				result += Dfs(node.left, min, max);
+			if (node.val <= max && node.val >= min)
+				result += node.val;
+			return result;
+		}
+	}
+}
