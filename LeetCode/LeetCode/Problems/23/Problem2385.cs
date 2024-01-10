086b21d133a5f1f1caf6189c4ba5bdeee72aaba9
Murrelken <danilbutin@gmail.com>
2024-01-11 00:49:53 +0100

    add 2385 csharp

---
+++ LeetCode/LeetCode/Problems/23/Problem2385.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.DataStructures;
+using System;
+
+namespace LeetCode.Problems._23
+{
+	internal class Problem2385
+	{
+		private int _result;
+		public int AmountOfTime(TreeNode root, int start)
+		{
+			DFS(root, start);
+			return _result;
+		}
+
+		private int DFS(TreeNode node, int start)
+		{
+			if (node == null) return 0;
+
+			int leftDepth = DFS(node.left, start);
+			int rightDepth = DFS(node.right, start);
+
+			if (node.val == start)
+			{
+				_result = Math.Max(leftDepth, rightDepth);
+				return -1;
+			}
+			else if (leftDepth >= 0 && rightDepth >= 0)
+				return Math.Max(leftDepth, rightDepth) + 1;
+
+			_result = Math.Max(_result, Math.Abs(leftDepth - rightDepth));
+			return Math.Min(leftDepth, rightDepth) - 1;
+		}
+	}
+}
