9f177b05ca4026a04f5c69abaf443a20ef9e9b76
Murrelken <danilbutin@gmail.com>
2024-01-17 21:12:01 +0100

    add 1207 csharp

---
+++ LeetCode/LeetCode/Problems/12/Problem1207.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._12
+{
+	internal class Problem1207
+	{
+		public bool UniqueOccurrences(int[] arr)
+		{
+			var dic = new Dictionary<int, int>();
+			foreach (int i in arr)
+			{
+				if (dic.ContainsKey(i))
+					dic[i]++;
+				else
+					dic.Add(i, 1);
+			}
+			var set = new HashSet<int>();
+			return dic.Values.Distinct().Count() == dic.Count;
+		}
+	}
+}
