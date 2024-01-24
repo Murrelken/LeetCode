32908d97051c506f954d1c44093d303ffa44f578
Murrelken <danilbutin@gmail.com>
2024-01-24 18:16:35 +0100

    add 1457 csharp

---
+++ LeetCode/LeetCode/Problems/14/Problem1457.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Problems._14
+{
+	internal class Problem1457
+	{
+		public int PseudoPalindromicPaths(TreeNode root)
+		{
+			return CountPseudoPalindromicPaths(root, 0);
+		}
+
+		private int CountPseudoPalindromicPaths(TreeNode node, int count)
+		{
+			if (node == null)
+				return 0;
+
+			count ^= 1 << (node.val - 1);
+
+			if (node.left == null && node.right == null)
+				return (count & (count - 1)) == 0 ? 1 : 0;
+
+			int leftPaths = CountPseudoPalindromicPaths(node.left, count);
+			int rightPaths = CountPseudoPalindromicPaths(node.right, count);
+
+			return leftPaths + rightPaths;
+		}
+	}
+}
