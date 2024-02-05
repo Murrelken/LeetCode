2bf45ef72c497d5aa8d48fb7213c8c7d17acaf9f
Murrelken <danilbutin@gmail.com>
2024-02-05 21:19:15 +0100

    add 387 csharp

---
+++ LeetCode/LeetCode/Problems/3/Problem387.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems._3
+{
+	internal class Problem387
+	{
+		public int FirstUniqChar(string s)
+		{
+			var map = new Dictionary<char, int>();
+            foreach (var item in s)
+                if (!map.TryAdd(item, 1))
+					map[item]++;
+
+			for (var i = 0; i < s.Length; i++)
+				if (map[s[i]] == 1)
+					return i;
+
+			return -1;
+		}
+	}
+}
