66ce8c089198112d25c3d83b2168a1f6d5bc6f92
Murrelken <danilbutin@gmail.com>
2024-02-06 22:58:26 +0100

    add csharp 49

---
+++ LeetCode/LeetCode/Problems/4/Problem49.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._4
+{
+	internal class Problem49
+	{
+		public IList<IList<string>> GroupAnagrams(string[] strs)
+		{
+			var dic = new Dictionary<string, IList<string>>();
+
+			foreach (var str in strs)
+			{
+				var chars = str.ToCharArray();
+				Array.Sort(chars);
+				var nstr = new string(chars);
+				if (!dic.TryAdd(nstr, new List<string> { str }))
+					dic[nstr].Add(str);
+			}
+
+			var result = dic.Values.ToList();
+			return result;
+		}
+	}
+}
