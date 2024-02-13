a14cc2dc9e1c4d3143a586725666aa39437e427f
Murrelken <danilbutin@gmail.com>
2024-02-13 22:02:57 +0100

    add 2108 csharp

---
+++ LeetCode/LeetCode/Problems/21/Problem2108.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems._21
+{
+	internal class Problem2108
+	{
+		public string FirstPalindrome(string[] words)
+		{
+			foreach (var w in words)
+			{
+				var l = 0;
+				var r = w.Length - 1;
+				while (l <= r)
+				{
+					if (w[l] != w[r])
+						break;
+					l++;
+					r--;
+				}
+				if (l > r)
+					return w;
+			}
+			return string.Empty;
+		}
+	}
+}
