814b9a3798a81cecda6be03957e9945ead07a2e4
Murrelken <danilbutin@gmail.com>
2024-02-15 01:10:28 +0100

    add 2149 csharp

---
+++ LeetCode/LeetCode/Problems/21/Problem2149.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Problems._21
+{
+	internal class Problem2149
+	{
+		public int[] RearrangeArray(int[] nums)
+		{
+			var result = new int[nums.Length];
+			var pos = 0;
+			var neg = 1;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] > 0)
+				{
+					result[pos] = nums[i];
+					pos += 2;
+				}
+				else
+				{
+					result[neg] = nums[i];
+					neg += 2;
+				}
+			}
+
+			return result;
+		}
+	}
+}
