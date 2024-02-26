228c9d667d11be4f10b6370fbc11b5b636b488b8
Murrelken <danilbutin@gmail.com>
2024-02-26 14:43:35 +0100

    add 100 csharp

---
+++ LeetCode/LeetCode/Problems/1/Problem100.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Problems._1
+{
+	internal class Problem100
+	{
+		public bool IsSameTree(TreeNode p, TreeNode q)
+		{
+			if (p == null && q == null)
+				return true;
+			if (p?.val != q?.val)
+				return false;
+			return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+		}
+	}
+}
