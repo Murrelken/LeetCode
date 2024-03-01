4525f0002da5c17131cb133f059de383a1a0d1a7
Murrelken <danilbutin@gmail.com>
2024-03-01 19:56:16 +0100

    add 2864 csharp

---
+++ LeetCode/LeetCode/Problems/28/Problem2864.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems._28
+{
+	internal class Problem2864
+	{
+		public string MaximumOddBinaryNumber(string s)
+		{
+			var ones = s.Count(x => x == '1');
+			var zeroes = s.Length - ones;
+			var sb = new StringBuilder();
+			while (ones > 1)
+			{
+				ones--;
+				sb.Append("1");
+			}
+			while (zeroes > 0)
+			{
+				zeroes--;
+				sb.Append("0");
+			}
+			sb.Append("1");
+			return sb.ToString();
+		}
+	}
+}
