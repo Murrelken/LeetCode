7020c5e507874f2aef7356f8dc2964f2109b83af
Murrelken <danilbutin@gmail.com>
2024-03-02 18:03:28 +0100

    add 977 csharp

---
+++ LeetCode/LeetCode/Problems/9/Problem977.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Problems._9
+{
+	internal class Problem977
+	{
+		public int[] SortedSquares(int[] nums)
+		{
+			var res = nums.Select(x => x * x).ToArray();
+			Array.Sort(res);
+			return res;
+		}
+	}
+}
