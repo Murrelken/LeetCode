8c2ca8e7f91901de89e6cb9e24a3e8628467cda9
Murrelken <danilbutin@gmail.com>
2024-03-04 21:27:38 +0100

    add 948 csharp

---
+++ LeetCode/LeetCode/Problems/9/Problem948.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.Problems._9
+{
+	internal class Problem948
+	{
+		public int BagOfTokensScore(int[] t, int power)
+		{
+			Array.Sort(t);
+			var n = t.Length;
+			var l = 0;
+			var r = n - 1;
+			var res = 0;
+
+			while (l < n)
+			{
+				if (t[l] <= power)
+				{
+					res++;
+					power -= t[l];
+					l++;
+				}
+				else if (l < r && t[l] <= power + t[r] && res > 0)
+				{
+					res--;
+					power += t[r];
+					r--;
+				}
+				else break;
+			}
+
+			return res;
+		}
+	}
+}
