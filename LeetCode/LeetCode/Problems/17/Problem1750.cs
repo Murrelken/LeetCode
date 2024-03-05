65ddb241bb64ff9b85bad8135103235bcd5dd0ed
Murrelken <danilbutin@gmail.com>
2024-03-05 23:24:19 +0100

    add 1750 csharp

---
+++ LeetCode/LeetCode/Problems/17/Problem1750.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems._17
+{
+	internal class Problem1750
+	{
+		public int MinimumLength(string s)
+		{
+			var n = s.Length;
+			var l = 0;
+			var r = n - 1;
+
+			while (l < r && s[l] == s[r])
+			{
+				var current = s[l];
+
+				while (l <= r && s[l] == current)
+					l++;
+				while (l <= r && s[r] == current)
+					r--;
+			}
+			return r - l + 1;
+		}
+	}
+}
