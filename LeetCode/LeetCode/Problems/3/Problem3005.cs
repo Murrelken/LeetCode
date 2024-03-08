a6a081d2e09a2ebac9da0c8cb9e0640ef0429a66
Murrelken <danilbutin@gmail.com>
2024-03-08 21:42:53 +0100

    add 3005 csharp

---
+++ LeetCode/LeetCode/Problems/3/Problem3005.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._3
+{
+	internal class Problem3005
+	{
+		public int MaxFrequencyElements(int[] nums)
+		{
+			var dic = new Dictionary<int, int>();
+			foreach (var n in nums)
+				if (!dic.TryAdd(n, 1))
+					dic[n]++;
+			var res = 0;
+			var counts = dic.Values.OrderByDescending(x => x);
+			var max = counts.First();
+			foreach (var c in counts)
+				if (c == max)
+					res += c;
+				else
+					break;
+			return res;
+		}
+	}
+}
