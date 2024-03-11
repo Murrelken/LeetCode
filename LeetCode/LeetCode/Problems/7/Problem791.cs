867046d7391ee8c50590d11ab3a133fb057d631d
Murrelken <danilbutin@gmail.com>
2024-03-11 20:36:28 +0100

    add 791 csharp

---
+++ LeetCode/LeetCode/Problems/7/Problem791.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace LeetCode.Problems._7
+{
+	internal class Problem791
+	{
+		public string CustomSortString(string order, string s)
+		{
+			var map = new int[26];
+			foreach (var c in s)
+				map[c - 'a']++;
+
+			var result = new StringBuilder();
+
+			foreach (var c in order)
+			{
+				result.Append(new string(c, map[c - 'a']));
+				map[c - 'a'] = 0;
+			}
+
+			for (int i = 0; i < 26; i++)
+				result.Append(new string((char)('a' + i), map[i]));
+
+			return result.ToString();
+		}
+	}
+}
