792c3b5ec5635be72968ac7e6ff85b419570304a
Murrelken <danilbutin@gmail.com>
2024-03-19 20:18:29 +0100

    add 621 csharp

---
+++ LeetCode/LeetCode/Problems/6/Problem621.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Problems._6
+{
+	internal class Problem621
+	{
+		public int LeastInterval(char[] tasks, int n)
+		{
+			int[] frequencies = new int[26];
+			foreach (char task in tasks)
+				frequencies[task - 'A']++;
+
+			Array.Sort(frequencies);
+
+			int maxFrequency = frequencies[25] - 1;
+			int idleSlots = maxFrequency * n;
+
+			for (int i = 24; i >= 0 && frequencies[i] > 0; i--)
+				idleSlots -= Math.Min(frequencies[i], maxFrequency);
+
+			return Math.Max(idleSlots, 0) + tasks.Length;
+		}
+	}
+}
