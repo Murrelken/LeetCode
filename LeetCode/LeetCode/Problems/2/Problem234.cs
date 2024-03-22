cb3f7ddad874f7b807f6a43a23ce584137cf6b62
Murrelken <danilbutin@gmail.com>
2024-03-22 13:14:16 +0100

    add 234 csharp

---
+++ LeetCode/LeetCode/Problems/2/Problem234.cs
@@ -0,0 +1,61 @@
+﻿using LeetCode.DataStructures;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems._2
+{
+	internal class Problem234
+	{
+		public bool IsPalindrome(ListNode head)
+		{
+			if (head.next == null)
+				return true;
+			var center = head;
+			var doubled = head.next;
+			bool even;
+
+			while (true)
+			{
+				if (doubled.next == null)
+				{
+					even = true;
+					break;
+				}
+
+				if (doubled.next.next == null)
+				{
+					even = false;
+					center = center.next;
+					break;
+				}
+				center = center.next;
+				doubled = doubled.next.next;
+			}
+
+			var firstHalfSb = new StringBuilder();
+
+			while (head != center)
+			{
+				firstHalfSb.Append(head.val);
+				head = head.next;
+			}
+
+			if (even)
+				firstHalfSb.Append(head.val);
+
+			var secondHalfSb = new StringBuilder();
+			center = center.next;
+
+			while (center != null)
+			{
+				secondHalfSb.Append(center.val);
+				center = center.next;
+			}
+
+			var f = new string(firstHalfSb.ToString().Reverse().ToArray());
+			var s = secondHalfSb.ToString();
+
+			return f == s;
+		}
+	}
+}
