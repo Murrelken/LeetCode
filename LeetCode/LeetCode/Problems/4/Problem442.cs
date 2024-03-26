c675b5a942e684e41fc108a041ff4b6c78856a8b
Murrelken <danilbutin@gmail.com>
2024-03-26 12:16:36 +0100

    add 442 csharp

---
+++ LeetCode/LeetCode/Problems/4/Problem442.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems._4
+{
+	internal class Problem442
+	{
+		public IList<int> FindDuplicates(int[] nums)
+		{
+			var unique = new HashSet<int>();
+			var res = new List<int>();
+			foreach (var n in nums)
+				if (!unique.Add(n))
+					res.Add(n);
+			return res;
+		}
+	}
+}
