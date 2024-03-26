a5782b9442f8230c082288f86983aa4b1ec9c766
Murrelken <danilbutin@gmail.com>
2024-03-26 12:52:06 +0100

    add 41 csharp

---
+++ LeetCode/LeetCode/Problems/4/Problem41.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LeetCode.Problems._4
+{
+	internal class Problem41
+	{
+		public int FirstMissingPositive(int[] nums)
+		{
+			var n = nums.Length;
+			var contains1 = false;
+
+			for (var i = 0; i < n; i++)
+			{
+				if (nums[i] == 1)
+					contains1 = true;
+				if (nums[i] <= 0 || nums[i] > n)
+					nums[i] = 1;
+			}
+
+			if (!contains1) return 1;
+
+			for (var i = 0; i < n; i++)
+			{
+				var value = Math.Abs(nums[i]);
+				if (value == n)
+					nums[0] = -Math.Abs(nums[0]);
+				else
+					nums[value] = -Math.Abs(nums[value]);
+			}
+
+			for (var i = 1; i < n; i++)
+				if (nums[i] > 0)
+					return i;
+
+			if (nums[0] > 0)
+				return n;
+
+			return n + 1;
+		}
+	}
+}
