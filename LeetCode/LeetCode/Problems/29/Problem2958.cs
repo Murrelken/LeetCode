2bcb6f0a060de48761c786e6f4dcee7ce932e9b0
Murrelken <danilbutin@gmail.com>
2024-03-28 15:10:29 +0100

    add 2958 csharp

---
+++ LeetCode/LeetCode/Problems/29/Problem2958.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems._29
+{
+	internal class Problem2958
+	{
+		public int MaxSubarrayLength(int[] nums, int k)
+		{
+			var left = 0;
+			var right = 0;
+			var res = 0;
+			var count = new Dictionary<int, int>();
+
+			while (right < nums.Length)
+			{
+				if (!count.TryAdd(nums[right], 1))
+					count[nums[right]]++;
+
+				if (count[nums[right]] > k)
+				{
+					res = Math.Max(res, right - left);
+					while (nums[left] != nums[right])
+					{
+						count[nums[left]]--;
+						left++;
+					}
+					count[nums[left]]--;
+					left++;
+				}
+
+				right++;
+			}
+
+			res = Math.Max(res, right - left);
+			return res;
+		}
+	}
+}
