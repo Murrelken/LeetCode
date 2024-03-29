1e6ad610b1764971a4684c0e82aff06c15151952
Murrelken <danilbutin@gmail.com>
2024-03-29 14:10:23 +0100

    add 2962 csharp

---
+++ LeetCode/LeetCode/Problems/29/Problem2962.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems._29
+{
+	internal class Problem2962
+	{
+		public long CountSubarrays(int[] elements, int threshold)
+		{
+			long arrayLength = elements.Length;
+			long maxElement = elements.Max();
+			long leftIndex = 0, rightIndex = 0, maxElementCount = 0, subarrayCount = 0;
+
+			while (rightIndex < arrayLength)
+			{
+				if (elements[rightIndex] == maxElement)
+					maxElementCount++;
+
+				if (maxElementCount >= threshold)
+					while (maxElementCount >= threshold)
+					{
+						subarrayCount += arrayLength - rightIndex;
+						if (elements[leftIndex] == maxElement)
+							maxElementCount--;
+						leftIndex++;
+					}
+
+				rightIndex++;
+			}
+			return subarrayCount;
+		}
+	}
+}
