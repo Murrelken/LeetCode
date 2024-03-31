caea6973715536d0e5c9f7d9ab42a0b27c3459b8
Murrelken <danilbutin@gmail.com>
2024-03-31 04:24:12 +0200

    add 2444 csharp

---
+++ LeetCode/LeetCode/Problems/24/Problem2444.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Problems._24
+{
+	internal class Problem2444
+	{
+		public long CountSubarrays(int[] nums, int minK, int maxK)
+		{
+			long res = 0;
+			int badIdx = -1, leftIdx = -1, rightIdx = -1;
+
+			for (int i = 0; i < nums.Length; ++i)
+			{
+				if (nums[i] < minK || nums[i] > maxK)
+					badIdx = i;
+
+				if (nums[i] == minK)
+					leftIdx = i;
+
+				if (nums[i] == maxK)
+					rightIdx = i;
+
+				res += Math.Max(0, Math.Min(leftIdx, rightIdx) - badIdx);
+			}
+
+			return res;
+		}
+	}
+}
