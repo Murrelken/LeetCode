17fcb12f67b2f320cb9e7ff49a9d68ccaf1321a7
Murrelken <danilbutin@gmail.com>
2024-04-01 15:56:29 +0200

    add 58 csharp

---
+++ LeetCode/LeetCode/Problems/5/Problem58.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Problems._5
+{
+	internal class Problem58
+	{
+		public int LengthOfLastWord(string s)
+		{
+			var n = s.Length;
+			var right = n - 1;
+
+			while (s[right] == ' ')
+				right--;
+
+			var res = 0;
+			while (right >= 0 && s[right] != ' ')
+			{
+				res++;
+				right--;
+			}
+
+			return res;
+		}
+	}
+}
