41b261970b4dcfb12b208c442066477b9645550c
Murrelken <danilbutin@gmail.com>
2024-04-02 22:36:58 +0200

    add 205 csharp

---
+++ LeetCode/LeetCode/Problems/2/Problem205.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._2
+{
+	internal class Problem205
+	{
+		public bool IsIsomorphic(string s, string t)
+		{
+			var sIndexesSum = new Dictionary<char, string>();
+			var tIndexesSum = new Dictionary<char, string>();
+
+			for (var i = 0; i < s.Length; i++)
+			{
+				if (!sIndexesSum.TryAdd(s[i], i.ToString()))
+					sIndexesSum[s[i]] += i.ToString();
+				if (!tIndexesSum.TryAdd(t[i], i.ToString()))
+					tIndexesSum[t[i]] += i.ToString();
+			}
+
+			var orderedS = sIndexesSum.Values.Order();
+			var orderedT = tIndexesSum.Values.Order();
+
+			return orderedS.SequenceEqual(orderedT);
+		}
+	}
+}
