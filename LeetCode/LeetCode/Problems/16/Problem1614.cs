303499a72aec516abaadfb96e21d179bdf06fbc4
Murrelken <danilbutin@gmail.com>
2024-04-04 17:24:01 +0200

    add 1614 csharp

---
+++ LeetCode/LeetCode/Problems/16/Problem1614.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Problems._16
+{
+	internal class Problem1614
+	{
+		public int MaxDepth(string s)
+		{
+			var max = 0;
+			var count = 0;
+
+			foreach (var c in s)
+			{
+				if (c == '(')
+				{
+					count++;
+					if (max < count)
+						max = count;
+				}
+				else if (c == ')')
+					count--;
+			}
+
+			return max;
+		}
+	}
+}
