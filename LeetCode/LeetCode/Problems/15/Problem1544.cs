f6196c7d2e37574c2244a8c761a63753917b4c50
Murrelken <danilbutin@gmail.com>
2024-04-05 11:43:05 +0200

    add 1544 csharp

---
+++ LeetCode/LeetCode/Problems/15/Problem1544.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._15
+{
+	internal class Problem1544
+	{
+		public string MakeGood(string s)
+		{
+			var stack = new Stack<char>();
+
+			foreach (var c in s)
+				if (stack.Any() && Math.Abs(c - stack.Peek()) == 32)
+					stack.Pop();
+				else
+					stack.Push(c);
+
+			var resultChars = stack.ToArray();
+			Array.Reverse(resultChars);
+
+			return new string(resultChars);
+		}
+	}
+}
