09bf4b1c607496e990be31720f6a88d2b29f2c41
Murrelken <danilbutin@gmail.com>
2024-04-07 23:33:02 +0200

    add 678

---
+++ LeetCode/LeetCode/Problems/6/Problem678.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problems._6
+{
+	internal class Problem678
+	{
+		public bool CheckValidString(string s)
+		{
+			var leftMin = 0;
+			var leftMax = 0;
+
+			foreach (char c in s)
+			{
+				if (c == '(')
+				{
+					leftMin++;
+					leftMax++;
+				}
+				else if (c == ')')
+				{
+					leftMin--;
+					leftMax--;
+				}
+				else
+				{
+					leftMin--;
+					leftMax++;
+				}
+				if (leftMax < 0) return false;
+				if (leftMin < 0) leftMin = 0;
+			}
+
+			return leftMin == 0;
+		}
+	}
+}
