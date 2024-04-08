77d68fd60fba76ab3abe7b8714fb8ad3fe2e99f1
Murrelken <danilbutin@gmail.com>
2024-04-08 22:20:58 +0200

    add 1700 csharp

---
+++ LeetCode/LeetCode/Problems/17/Problem1700.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems._17
+{
+	internal class Problem1700
+	{
+		public int CountStudents(int[] students, int[] sandwiches)
+		{
+			var count = new int[2];
+
+			foreach (var student in students)
+				count[student]++;
+
+			var i = 0;
+			while (i < sandwiches.Length)
+			{
+				if (count[sandwiches[i]] == 0)
+					break;
+
+				count[sandwiches[i]]--;
+
+				i++;
+			}
+
+			return students.Length - i;
+		}
+	}
+}
