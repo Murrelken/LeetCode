a8afe47bbb2b3b9e18b433cda61a50d0b8ad46f0
Murrelken <danilbutin@gmail.com>
2024-04-09 16:17:01 +0200

    add 2073 csharp

---
+++ LeetCode/LeetCode/Problems/20/Problem2073.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Problems._20
+{
+	internal class Problem2073
+	{
+		public int TimeRequiredToBuy(int[] tickets, int k)
+		{
+			var kth = tickets[k];
+			var res = 0;
+
+			for (var i = 0; i <= k; i++)
+				res += Math.Min(tickets[i], kth);
+
+			for (var i = k + 1; i < tickets.Length; i++)
+				res += Math.Min(tickets[i], kth - 1);
+
+			return res;
+		}
+	}
+}
