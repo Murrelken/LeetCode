3deb67e923d1a53727233068f8122b42e716b1af
Murrelken <danilbutin@gmail.com>
2024-04-10 18:55:53 +0200

    add 950 csharp

---
+++ LeetCode/LeetCode/Problems/9/Problem950.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems._9
+{
+	internal class Problem950
+	{
+		public int[] DeckRevealedIncreasing(int[] deck)
+		{
+			Array.Sort(deck);
+			var indexQueue = new Queue<int>(Enumerable.Range(0, deck.Length));
+			var result = new int[deck.Length];
+
+			foreach (int card in deck)
+			{
+				result[indexQueue.Dequeue()] = card;
+				if (indexQueue.Count > 0)
+					indexQueue.Enqueue(indexQueue.Dequeue());
+			}
+
+			return result;
+		}
+	}
+}
