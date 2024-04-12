3299d11848c05e1b54340a53565c4cc51d6b1475
Murrelken <danilbutin@gmail.com>
2024-04-12 20:31:18 +0200

    add 42 csharp

---
+++ LeetCode/LeetCode/Problems/4/Problem42.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Problems._4
+{
+	internal class Problem42
+	{
+		public int Trap(int[] height)
+		{
+			var n = height.Length;
+			if (n == 0) return 0;
+
+			var left = 0;
+			var right = n - 1;
+			var leftMax = 0;
+			var rightMax = 0;
+			var waterTrapped = 0;
+
+			while (left < right)
+			{
+				if (height[left] < height[right])
+				{
+					if (height[left] >= leftMax)
+						leftMax = height[left];
+					else
+						waterTrapped += leftMax - height[left];
+					left++;
+				}
+				else
+				{
+					if (height[right] >= rightMax)
+						rightMax = height[right];
+					else
+						waterTrapped += rightMax - height[right];
+					right--;
+				}
+			}
+
+			return waterTrapped;
+		}
+	}
+}
