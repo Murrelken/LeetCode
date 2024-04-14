5f5a90ea31abe556a1f7ceb6d61feb960e1006ee
Murrelken <danilbutin@gmail.com>
2024-04-14 11:32:56 +0200

    add 404 csharp

---
+++ LeetCode/LeetCode/Problems/4/Problem404.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Problems._4
+{
+	internal class Problem404
+	{
+		public int SumOfLeftLeaves(TreeNode root) => Help(root, false);
+
+		private int Help(TreeNode node, bool isLeft)
+		{
+			if (node == null) return 0;
+
+			if (isLeft && node is { left: null, right: null }) return node.val;
+
+			return Help(node.left, true) + Help(node.right, false);
+		}
+	}
+}
