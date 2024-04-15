981d6d401bb93a19d96446f30577c342c127bef7
Murrelken <danilbutin@gmail.com>
2024-04-15 22:48:19 +0200

    add 129 csharp

---
+++ LeetCode/LeetCode/Problems/1/Problem129.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Problems._1
+{
+	internal class Problem129
+	{
+		public int SumNumbers(TreeNode root) => Dfs(root, 0);
+
+		private int Dfs(TreeNode node, int num)
+		{
+			if (node == null) return 0;
+			if (node.left == null && node.right == null)
+				return num * 10 + node.val;
+			return Dfs(node.left, num * 10 + node.val) + Dfs(node.right, num * 10 + node.val);
+		}
+	}
+}
