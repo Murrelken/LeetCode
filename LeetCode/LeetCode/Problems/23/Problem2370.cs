7900461ac4926b83f0cb287560a5770c4120e633
Murrelken <danilbutin@gmail.com>
2024-04-25 23:38:56 +0200

    add 2370 csharp

---
+++ LeetCode/LeetCode/Problems/23/Problem2370.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Problems._23
+{
+	internal class Problem2370
+	{
+		public int LongestIdealString(string s, int k)
+		{
+			var n = s.Length;
+			var dp = new int[26];
+			var maxLen = 0;
+
+			for (var i = 0; i < n; i++)
+			{
+				var curr = s[i];
+				var idx = curr - 'a';
+				var best = 0;
+
+				for (var prev = 0; prev < 26; prev++)
+					if (Math.Abs(curr - 'a' - prev) <= k)
+						best = Math.Max(best, dp[prev]);
+
+				dp[idx] = best + 1;
+				maxLen = Math.Max(maxLen, dp[idx]);
+			}
+
+			return maxLen;
+		}
+	}
+}
