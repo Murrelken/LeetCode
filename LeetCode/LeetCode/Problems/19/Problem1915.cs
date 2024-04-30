a3ddae1203a38c99fd3d9c2c568cc02d7827ea07
Murrelken <danilbutin@gmail.com>
2024-05-01 01:39:54 +0200

    add 1915 csharp

---
+++ LeetCode/LeetCode/Problems/19/Problem1915.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Problems._19
+{
+	internal class Problem1915
+	{
+		public long WonderfulSubstrings(string word)
+		{
+			var result = 0;
+			var count = new int[1024];
+			count[0] = 1;
+			var mask = 0;
+
+			foreach (char c in word)
+			{
+				mask ^= 1 << (c - 'a');
+				result += count[mask];
+				for (var i = 0; i < 10; i++)
+					result += count[mask ^ (1 << i)];
+				count[mask]++;
+			}
+
+			return result;
+		}
+	}
+}
