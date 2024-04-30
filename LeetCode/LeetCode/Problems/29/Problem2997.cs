edc2fcec1adb597e6f1385d1df5a07249ca540fc
Murrelken <danilbutin@gmail.com>
2024-05-01 01:39:43 +0200

    add 2997 csharp

---
+++ LeetCode/LeetCode/Problems/29/Problem2997.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems._29
+{
+	internal class Problem2997
+	{
+		public int MinOperations(int[] nums, int k)
+		{
+			int operations = 0;
+			int xor = 0;
+
+			foreach (var num in nums)
+				xor ^= num;
+
+			if (xor == k)
+				return 0;
+
+			int target = xor ^ k;
+
+			while (target > 0)
+			{
+				operations++;
+				target &= (target - 1);
+			}
+
+			return operations;
+		}
+	}
+}
