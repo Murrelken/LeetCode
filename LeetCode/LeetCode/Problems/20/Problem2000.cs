d5040251d3d2907887284b2d893743dcc322db7d
Murrelken <danilbutin@gmail.com>
2024-05-01 23:54:48 +0200

    add 2000 csharp

---
+++ LeetCode/LeetCode/Problems/20/Problem2000.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems._20
+{
+	internal class Problem2000
+	{
+		public string ReversePrefix(string word, char ch)
+		{
+			var index = word.IndexOf(ch);
+			if (index == -1)
+				return word;
+
+			var chars = word.ToCharArray();
+			var start = 0;
+			var end = index;
+
+			while (start < end)
+			{
+				var temp = chars[start];
+				chars[start] = chars[end];
+				chars[end] = temp;
+				start++;
+				end--;
+			}
+
+			return new string(chars);
+		}
+	}
+}
