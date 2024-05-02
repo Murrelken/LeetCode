86da7fe6f4a041738e40ada6b06a3498da7a8292
Murrelken <danilbutin@gmail.com>
2024-05-02 23:29:11 +0200

    add 2441 csharp

---
+++ LeetCode/LeetCode/Problems/24/Problem2441.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Problems._24
+{
+	internal class Problem2441
+	{
+		public int FindMaxK(int[] nums)
+		{
+			Array.Sort(nums);
+			var maxK = -1;
+
+			var left = 0;
+			var right = nums.Length - 1;
+			while (left < right)
+			{
+				var sum = nums[left] + nums[right];
+				if (sum == 0)
+				{
+					maxK = Math.Max(maxK, Math.Abs(nums[right]));
+					left++;
+					right--;
+				}
+				else if (sum < 0)
+					left++;
+				else
+					right--;
+			}
+
+			return maxK;
+		}
+	}
+}
