ea2fdc97951b902114f4983f7451a985616eb6cb
Murrelken <danilbutin@gmail.com>
2024-05-04 00:34:14 +0200

    add 165 csharp

---
+++ LeetCode/LeetCode/Problems/1/Problem165.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Problems._1
+{
+	internal class Problem165
+	{
+		public int CompareVersion(string version1, string version2)
+		{
+			var i = 0;
+			var j = 0;
+
+			while (i < version1.Length || j < version2.Length)
+			{
+				var num1 = 0;
+				var num2 = 0;
+
+				while (i < version1.Length && version1[i] != '.')
+				{
+					num1 = num1 * 10 + (version1[i] - '0');
+					i++;
+				}
+
+				while (j < version2.Length && version2[j] != '.')
+				{
+					num2 = num2 * 10 + (version2[j] - '0');
+					j++;
+				}
+
+				if (num1 < num2) return -1;
+				else if (num1 > num2) return 1;
+
+				i++;
+				j++;
+			}
+
+			return 0;
+		}
+	}
+}
