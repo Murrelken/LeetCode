a98564e169b693a60a59ad829905df12e0172092
Murrelken <danilbutin@gmail.com>
2024-05-04 23:07:45 +0200

    add 881 csharp

---
+++ LeetCode/LeetCode/Problems/8/Problem881.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Problems._8
+{
+	internal class Problem881
+	{
+		public int NumRescueBoats(int[] people, int limit)
+		{
+			Array.Sort(people);
+			var boats = 0;
+			var left = 0;
+			var right = people.Length - 1;
+
+			while (left <= right)
+			{
+				if (people[left] + people[right] <= limit)
+					left++;
+				right--;
+				boats++;
+			}
+
+			return boats;
+		}
+	}
+}
