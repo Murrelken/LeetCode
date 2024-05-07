78030b8d0a4e7ed0b796b16c79bbc8e5cacef792
Murrelken <danilbutin@gmail.com>
2024-05-08 00:02:50 +0200

    add 2816 csharp

---
+++ LeetCode/LeetCode/Problems/28/Problem2816.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Problems._28
+{
+	internal class Problem2816
+	{
+		public ListNode DoubleIt(ListNode head)
+		{
+			var prev = RR(head);
+			if (prev == 1)
+				head = new ListNode(1, head);
+			return head;
+		}
+
+		private static int RR(ListNode node)
+		{
+			var previous = 0;
+			if (node.next != null)
+				previous = RR(node.next);
+
+			var newCurrent = node.val * 2 + previous;
+
+			node.val = newCurrent % 10;
+			return newCurrent / 10;
+		}
+	}
+}
