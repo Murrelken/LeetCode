e7bc54ba61df19ef92b4a63cc98a2c37141eaf51
Murrelken <danilbutin@gmail.com>
2024-05-07 22:49:13 +0200

    add 2487 csharp

---
+++ LeetCode/LeetCode/Problems/24/Problem2487.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.DataStructures;
+
+namespace LeetCode.Problems._24
+{
+	internal class Problem2487
+	{
+		public ListNode RemoveNodes(ListNode head) => RR(head);
+
+		private static ListNode RR(ListNode node)
+		{
+			if (node.next == null)
+				return node;
+
+			var next = RR(node.next);
+
+			if (next.val <= node.val)
+			{
+				node.next = next;
+				return node;
+			}
+			else
+				return next;
+		}
+	}
+}
