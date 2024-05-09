4f102710153ae964a6a60e91086c57e5d3a7a54f
Murrelken <danilbutin@gmail.com>
2024-05-10 00:57:59 +0200

    add 3075 csharp

---
+++ LeetCode/LeetCode/Problems/3/Problem3075.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Problems._3
+{
+	internal class Problem3075
+	{
+		public long MaximumHappinessSum(int[] happiness, int k)
+		{
+			Array.Sort(happiness);
+			Array.Reverse(happiness);
+
+			var rounds = 0;
+			var totalHappySum = 0;
+
+			for (var i = 0; i < k && i < happiness.Length; i++)
+			{
+				var selected = happiness[i] - rounds;
+				if (selected > 0)
+				{
+					totalHappySum += selected;
+					rounds++;
+				}
+			}
+
+			return totalHappySum;
+		}
+	}
+}
