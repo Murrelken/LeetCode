f380a3df4f4b98d37916de4521ed85f131d31aa4
Murrelken <danilbutin@gmail.com>
2024-06-03 19:39:35 +0200

    add 2486 csharp

---
+++ LeetCode/LeetCode/Problems/24/Problem2486.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems._24
+{
+	internal class Problem2486
+	{
+		public int AppendCharacters(string s, string t)
+		{
+			var ti = 0;
+			var si = 0;
+
+			while (si < s.Length && ti < t.Length)
+			{
+				if (s[si] == t[ti])
+					ti++;
+				si++;
+			}
+
+			return t.Length - ti;
+		}
+	}
+}
