78c6464c0259e0053fdbb494fbcfdf76547031c4
Murrelken <danilbutin@gmail.com>
2024-06-04 22:41:17 +0200

    add 409 csharp

---
+++ LeetCode/LeetCode/Problems/4/Problem409.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems._4
+{
+	internal class Problem409
+	{
+		public int LongestPalindrome(string s)
+		{
+			var charCount = new int[128];
+
+			foreach (char c in s)
+				charCount[c]++;
+
+			var length = 0;
+			var hasOddCount = false;
+
+			foreach (var count in charCount)
+				if (count % 2 == 0)
+					length += count;
+				else
+				{
+					length += count - 1;
+					hasOddCount = true;
+				}
+
+			if (hasOddCount)
+				length += 1;
+
+			return length;
+		}
+	}
+}
