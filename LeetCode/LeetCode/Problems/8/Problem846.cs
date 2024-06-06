f405a6bb4e10b858784cbd3d9bc1506c59c3137c
Murrelken <danilbutin@gmail.com>
2024-06-06 23:56:30 +0200

    add 846 csharp

---
+++ LeetCode/LeetCode/Problems/8/Problem846.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems._8
+{
+	internal class Problem846
+	{
+		public bool IsNStraightHand(int[] hand, int groupSize)
+		{
+			if (hand.Length % groupSize != 0)
+				return false;
+
+			Array.Sort(hand);
+
+			var cardCount = new Dictionary<int, int>();
+			foreach (var card in hand)
+				if (cardCount.ContainsKey(card))
+					cardCount[card]++;
+				else
+					cardCount[card] = 1;
+
+			foreach (var card in hand)
+			{
+				if (cardCount[card] == 0)
+					continue;
+
+				for (var i = 0; i < groupSize; i++)
+				{
+					var currentCard = card + i;
+					if (!cardCount.ContainsKey(currentCard) || cardCount[currentCard] == 0)
+						return false;
+					cardCount[currentCard]--;
+				}
+			}
+
+			return true;
+		}
+	}
+}
