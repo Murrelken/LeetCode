9164880cc6ad48d4d13347a5106626f587c1ce4c
Murrelken <danilbutin@gmail.com>
2024-06-10 00:49:01 +0200

    add 974 csharp

---
+++ LeetCode/LeetCode/Problems/9/Problem974.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems._9
+{
+	internal class Problem974
+	{
+		public int SubarraysDivByK(int[] nums, int k)
+		{
+			var count = 0;
+			var prefixSum = 0;
+			var remainderCounts = new Dictionary<int, int>
+			{
+				[0] = 1
+			};
+
+			foreach (var num in nums)
+			{
+				prefixSum += num;
+				var mod = ((prefixSum % k) + k) % k;
+
+				if (remainderCounts.ContainsKey(mod))
+				{
+					count += remainderCounts[mod];
+					remainderCounts[mod]++;
+				}
+				else
+					remainderCounts[mod] = 1;
+			}
+
+			return count;
+		}
+	}
+}
