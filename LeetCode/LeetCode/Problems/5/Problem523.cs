06d6732ce0ed00ed9ee99ad94d56d9184b9048d4
Murrelken <danilbutin@gmail.com>
2024-06-09 02:15:12 +0200

    add 523 csharp

---
+++ LeetCode/LeetCode/Problems/5/Problem523.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+
+namespace LeetCode.Problems._5
+{
+	internal class Problem523
+	{
+		public bool CheckSubarraySum(int[] nums, int k)
+		{
+			var remainderMap = new Dictionary<int, int>();
+			remainderMap[0] = -1;
+
+			int runningSum = 0;
+
+			for (var i = 0; i < nums.Length; i++)
+			{
+				runningSum += nums[i];
+
+				var remainder = runningSum % k;
+
+				if (remainder < 0)
+					remainder += k;
+
+				if (remainderMap.ContainsKey(remainder))
+				{
+					if (i - remainderMap[remainder] > 1)
+						return true;
+				}
+				else
+					remainderMap[remainder] = i;
+			}
+
+			return false;
+		}
+	}
+}
--- leetcode_rust/src/main.rs
+++ leetcode_rust/src/main.rs
@@ -1,10 +1,30 @@
-mod problems {
-    pub mod problem_2142;
-}
+#![allow(dead_code, unused_imports)]
+mod problems;
+mod structures;
+
+use crate::problems::problem_295::MedianFinder;
+use problems::*;
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::sync::Mutex;
+use structures::list_node::ListNode;
 
 fn main() {
-    let input = Vec::from([10, 10, 3, 5]);
+    //let input = gobln(1,gobln(4,gobln(3,gobln(2,gobln(5,gobln(2, None))))));
+
+    // let mut input = vec!["flower", "flow", "flight"];
+    // let input: Vec<String> = input.iter().map(|s| String::from(*s)).collect();
+
+    // let mut input = vec![[1, 2, 2], [3, 8, 2], [5, 3, 5], [1, 2, 2]];
+    // let input = input.iter().map(|x| x.to_vec()).collect();
+
+    let input = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
     println!("{:?}", input);
-    let res = problems::problem_2142::max_run_time(3, input);
-    println!("{res}");
+
+    let res = problem_1356::sort_by_bits(input);
+    dbg!(res);
+}
+
+fn gobln(val: i32, next: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    Some(Box::new(ListNode::new_with_next(val, next)))
 }
